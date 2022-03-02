06e46bc230e374341136be41ccc672330afc9217
daniesquivelc97 <dani_esquivelc@hotmail.com>
2022-03-02 17:01:30 -0500

    Aplicación para reproducir música

---
+++ lib/src/models/audioplayer_model.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AudioPlayerModel with ChangeNotifier {
+  bool _playing = false;
+  Duration _songDuration = const Duration(milliseconds: 0);
+  Duration _currentDuration = const Duration(milliseconds: 0);
+  late AnimationController _controller;
+
+  String get songTotalDuration => printDuration(_songDuration);
+  String get currentSecond => printDuration(_currentDuration);
+
+  double get porcentaje => (_songDuration.inSeconds > 0)
+      ? _currentDuration.inSeconds / _songDuration.inSeconds
+      : 0;
+
+  AnimationController get controller => _controller;
+
+  set controller(AnimationController valor) {
+    _controller = valor;
+  }
+
+  bool get playing => _playing;
+
+  set playing(bool valor) {
+    _playing = valor;
+    notifyListeners();
+  }
+
+  Duration get songDuration => _songDuration;
+
+  set songDuration(Duration valor) {
+    _songDuration = valor;
+    notifyListeners();
+  }
+
+  Duration get currentDuration => _currentDuration;
+
+  set currentDuration(Duration valor) {
+    _currentDuration = valor;
+    notifyListeners();
+  }
+
+  String printDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
+  }
+}
